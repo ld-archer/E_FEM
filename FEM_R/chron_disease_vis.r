31bbcd7142b8916c27ff15b1c2c1039e5537a91e
ld-archer <l.archer@leeds.ac.uk>
2020-06-25 10:44:52 +0100

    Simple R script for visualising FEM outputs (WIP)

--- FEM_R/chron_disease_vis.r
+++ FEM_R/chron_disease_vis.r
@@ -14,3 +14,24 @@ baseCancer <- baseline %>% select(contains(c('year', 'cancre')))
 plot(baseCancer$year, baseCancer$p_cancre_all, type='l', col='red')
 lines(pCancer10$year, pCancer10$p_cancre_all, col='green')
 legend(2012, 0.18,legend=c("Year", "Prevalence of Cancer"), col=c("red", "green"))
+
+
+cohort <- read_dta('output/ELSA_cohort/ELSA_cohort_summary.dta')
+
+commit <- read_dta('output/COMMIT_cSmoken/COMMIT_cSmoken_summary.dta')
+
+plot(cohort$year, cohort$n_smoken_all, type='l', col='red')
+lines(commit$year, commit$n_smoken_all, col='green')
+
+plot(commit$year, commit$p_smoken_all, type='l', col='green')
+lines(cohort$year, cohort$p_smoken_all, col='red')
+
+plot(commit$year, commit$n_lunge_all, type='l', col='green')
+lines(cohort$year, cohort$n_lunge_all, col='red')
+
+plot(commit$year, commit$p_cancre_all, type='l', col='green')
+lines(cohort$year, cohort$p_cancre_all, col='red')
+
+plot(commit$year, commit$p_diabe_all, type='l', col='green')
+lines(cohort$year, cohort$p_diabe_all, col='red')
+
